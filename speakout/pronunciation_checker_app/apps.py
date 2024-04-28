8cbf9a3135e51e105799c8f68f2ec4c105c6a79c
atu <npatu07@gmail.com>
2024-04-28 16:40:36 +0700

    Refine project structure

---
+++ speakout/pronunciation_checker_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PronunciationCheckerAppConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "pronunciation_checker_app"
